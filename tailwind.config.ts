9769a860e0615376d8b78aac34317fb13fa0bf63
Ryan Fan <ryanfan1996@gmail.com>
2024-08-10 16:34:43 -0400

    font fix final

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   ],
   theme: {
     fontFamily: {
-      'display': ['Gradient Bold', 'Helvetica', 'san-serif'],
+      'display': ['Gradient', 'Helvetica', 'san-serif'],
       // 'sans': ['Graphik', 'Helvetica', 'san-serif'],
       'sans': ['neue-haas-unica', 'Helvetica', 'san-serif'],
     },
